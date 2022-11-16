3cc9e6dd09e9829a494cddf11b9fecad046820e8
Ellinor <ellinor.jakobsson@ebc.uu.se>
2022-11-16 08:00:32 +0100

    Added phase plane plot.

--- Population- and community ecology 2022/predator-prey model 1_2 levels.R
+++ Population- and community ecology 2022/predator-prey model 1_2 levels.R
@@ -37,13 +37,30 @@ ggplot(data = LV.out, aes(x = time, y = Population_size, linetype = `Trophic lev
   labs(x = "Time", y = "Population") +
   scale_linetype_discrete(labels=c('Prey', 'Predator'))
 
-# ####Phase plane diagram####
-# plot(LV.out[, 2], LV.out[, 3], type = "l",  lty = 1, col = "red", ylab = "consumer", xlab = "resource")
-# points(R0, C0, cex = 1.5, pch = 19, col = "red") # adds a dot to show the starting density
-# lines(LV.out2[, 2], LV.out2[, 3], lty = 1, col = "blue") # adds the second simulation to the plot
-# points(R02, C02, cex = 1.5 ,pch = 19, col = "blue") # adds a dot to show the starting density
-# lines(LV.out3[, 2], LV.out3[, 3], lty = 1, col = "green") # adds the third simulation to the plot
-# points(R03, C03, cex = 1.5, pch = 19, col = "green") # adds a dot to show the starting density
-# legend("topright", c(expression("simulation 1"), expression("simulation 2"), expression("simulation 3")), lty = 1, col = c("red","blue","green"), bty = "n")
-# abline(h = r/a, lty = 2) # iso-cline for the resource, which is a horizontal line
-# abline(v = d/(e * a), lty = 2) # iso-cline for the consumer, which is a vertical line
+####Phase plane diagram####
+plot(LV.out[, 2], LV.out[, 3], type = "l",  lty = 1, col = "red", ylab = "consumer", xlab = "resource")
+points(R0, C0, cex = 1.5, pch = 19, col = "red") # adds a dot to show the starting density
+lines(LV.out2[, 2], LV.out2[, 3], lty = 1, col = "blue") # adds the second simulation to the plot
+points(R02, C02, cex = 1.5 ,pch = 19, col = "blue") # adds a dot to show the starting density
+lines(LV.out3[, 2], LV.out3[, 3], lty = 1, col = "green") # adds the third simulation to the plot
+points(R03, C03, cex = 1.5, pch = 19, col = "green") # adds a dot to show the starting density
+legend("topright", c(expression("simulation 1"), expression("simulation 2"), expression("simulation 3")), lty = 1, col = c("red","blue","green"), bty = "n")
+abline(h = r/a, lty = 2) # iso-cline for the resource, which is a horizontal line
+abline(v = d/(e * a), lty = 2) # iso-cline for the consumer, which is a vertical line
+
+R02 <- 4
+C02 <- 8
+params1 <- c(r = r, a = a, e = e, d = d, k = k)
+MaxTime <- 50 # length of the time series, can be changed at any time
+Time <- seq(0, MaxTime, by = 0.1) # here we produce a sequence of time points at which we will determine the population densities
+LV.out2 <- ode(c(R02, C02), Time, predpreyLV, params1) # here we call the function "ode" that does the hard work of numerical integration and which is part of the package "deSolve"
+
+R03 <- 4
+C03 <- 8
+params1 <- c(r = r, a = a, e = e, d = d, k = k)
+MaxTime <- 50 # length of the time series, can be changed at any time
+Time <- seq(0, MaxTime, by = 0.1) # here we produce a sequence of time points at which we will determine the population densities
+LV.out2 <- ode(c(R03, C03), Time, predpreyLV, params1) # here we call the function "ode" that does the hard work of numerical integration and which is part of the package "deSolve"
+
+
+
