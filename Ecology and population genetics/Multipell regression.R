dbd4d27d748f06219682d2105ddcbde38d7a7939
Ellinor <ellinor.jakobsson@ebc.uu.se>
2023-11-15 18:38:30 +0100

    Modified file with comments

--- Ecology and population genetics/Multipell regression.R
+++ Ecology and population genetics/Multipell regression.R
@@ -1,16 +1,19 @@
 
+# SCript for running a simple multiple regression
 library(readxl)
 wd <- ""
 setwd(wd)
 data <- read_xlsx("datafile.xlsx")
+
+# Write model
 model <- lm(scale(response)~(scale(explanatory)) + (scale(explanatory)), data=data)
 
-#Normailty
+# Check assumption of normality
 aov_residuals <- residuals(object = model)
 shapiro.test(x = aov_residuals)
 summary(model)
 
-#plotting
+# Plot the data
 ggplot(data, aes(x=explanatory, y=response)) + geom_point() + theme_bw()
 ggplot(data, aes(x=explanatory, y=response)) + geom_point() + theme_bw()
 
